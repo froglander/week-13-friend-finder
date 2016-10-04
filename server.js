6f3e3458b6820b5e5f182031b9312227ea848e80
Kristi Heredia <froglander@dukworld.net>
2016-10-04 00:03:00 -0400

    Clean up server.js

--- server.js
+++ server.js
@@ -17,30 +17,12 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
 app.use(express.static('app/public'));
-//app.use(express.static(path.join(__dirname, '/public')));
-//app.use(express.static(path.join(process.env.PWD, '/public')));
-//app.use('/public', express.static(process.env.PWD + '/public'));
-// app.use('/public', express.static(path.join(process.env.PWD, 'public')));
-
-//app.use('/images',express.static(path.join(__dirname, 'public/assets/images')));
-//app.use('/css',express.static(path.join(__dirname, 'public/assets/css')));
-
-// app.use(express.static(__dirname + '/public'));
-// app.use('/static', express.static(__dirname + '/public'));
-
-console.log("dirname:", __dirname);
-console.log("process.env.PWD:", process.env.PWD);
 
 // Points the server to a series of 'route' files
 // that give the server a map of how to respond when users
 // visit or request data from various urls
 require('./app/routing/api-routes.js')(app);
 require('./app/routing/html-routes.js')(app);
-
-
-
-
-
 
 
 // Starts the server to begin listening
