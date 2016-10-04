d2b3960b62afee23cc49ca538e301fb2a9a2a456
Kristi Heredia <froglander@dukworld.net>
2016-10-03 23:40:24 -0400

    Trying to fix path issue with heroku

--- server.js
+++ server.js
@@ -16,12 +16,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
-//app.use(express.static('app/public'));
-app.use('/public', express.static(process.env.PWD + '/public'));
+app.use(express.static('app/public'));
+//app.use('/public', express.static(process.env.PWD + '/public'));
+// app.use('/public', express.static(path.join(process.env.PWD, 'public')));
+
+//app.use('/images',express.static(path.join(__dirname, 'public/assets/images')));
+//app.use('/css',express.static(path.join(__dirname, 'public/assets/css')));
+
 // app.use(express.static(__dirname + '/public'));
 // app.use('/static', express.static(__dirname + '/public'));
 
-console.log(process.env.PWD);
+//console.log(path);
 
 // Points the server to a series of 'route' files
 // that give the server a map of how to respond when users
